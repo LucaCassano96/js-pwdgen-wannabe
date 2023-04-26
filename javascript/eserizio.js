8d1b083f3adc38d15c554b03a983a34876a8eda3
Luca Cassano <lucacassano96@gmail.com>
2023-04-26 18:04:51 +0200

    completato

---
+++ javascript/eserizio.js
@@ -0,0 +1,41 @@
+
+/* Chiedi all’utente il suo nome  */
+
+let userName, userSurname, userColor, messaggioName, messaggioSurname, messaggioColor;
+
+userName = prompt ("inserisci il tuo nome");
+console.log(userName);
+
+
+
+/* poi chiedi il suo cognome */
+
+userSurname = prompt ("inserisci il tuo cognome");
+console.log(userSurname);
+
+
+
+
+/* poi chiedi il suo colore preferito */
+
+userColor = prompt ("inserisci il tuo colore preferito");
+console.log(userColor);
+
+
+/* messaggio all'utente */
+
+messaggioName =`Ti chiami: ${userName}`;
+messaggioSurname= `Il tuo cognome è: ${userSurname}`;
+messaggioColor= `Il tuo colore preferito è il: ${userColor}`
+
+console.log(messaggioName);
+console.log(messaggioSurname);
+console.log(messaggioColor);
+
+document.getElementById("prova-1").innerHTML = messaggioName;
+document.getElementById("prova-2").innerHTML = messaggioSurname;
+document.getElementById("prova-3").innerHTML = messaggioColor;
+
+
+
+
